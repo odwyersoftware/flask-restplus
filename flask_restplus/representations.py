df4302b3586f1f50443289ec68d3fdd71b03398a
Richard O'Dwyer <richard@richard.do>
2022-09-21 11:54:30 +0300

    fix json dump error

--- flask_restplus/representations.py
+++ flask_restplus/representations.py
@@ -6,6 +6,8 @@ try:
 except ImportError:
     from json import dumps
 
+from marshmallow.schema import Schema
+
 from flask import make_response, current_app
 
 
@@ -22,6 +24,14 @@ def output_json(data, code, headers=None):
 
     # always end the json dumps with a new line
     # see https://github.com/mitsuhiko/flask/pull/1262
+
+    # Fix for object is not serializable error.
+    if (
+        isinstance(data, dict) and
+        'schema' in data and
+        isinstance(data['schema'], Schema)
+    ):
+        data.pop('schema', None)
     dumped = dumps(data, **settings) + "\n"
 
     resp = make_response(dumped, code)
